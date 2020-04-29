0c90dcb30d0283fceb3fa136fa53adc0fd54e520
Diego Leal <dalc93@gmail.com>
2020-04-28 22:06:24 -0500

    Se agrega Spring HATEOAS

--- src/main/java/com/example/restservice/controller/EmployeeController.java
+++ src/main/java/com/example/restservice/controller/EmployeeController.java
@@ -2,37 +2,51 @@ package com.example.restservice.controller;
 
 import com.example.restservice.model.Employee;
 import com.example.restservice.repository.EmployeeRepository;
+import com.example.restservice.util.EmployeeModelAssembler;
 import com.example.restservice.util.EmployeeNotFoundException;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import org.springframework.hateoas.CollectionModel;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class EmployeeController {
 
     private EmployeeRepository employeeRepository;
 
-    EmployeeController(EmployeeRepository employeeRepository){
+    private EmployeeModelAssembler employeeModelAssembler;
+
+
+    EmployeeController(EmployeeRepository employeeRepository,  EmployeeModelAssembler employeeModelAssembler){
         this.employeeRepository = employeeRepository;
+        this.employeeModelAssembler = employeeModelAssembler;
     }
 
     //Aggregate Root
 
     @GetMapping(value = "/employees")
-    List<Employee> allEmployees(){
-        return this.employeeRepository.findAll();
+    public CollectionModel<EntityModel<Employee>> allEmployees(){
+        List<EntityModel<Employee>> employees = this.employeeRepository.findAll().stream()
+                .map(employeeModelAssembler::toModel)
+                .collect(Collectors.toList());
+        return new CollectionModel<>(employees,
+        linkTo(methodOn(EmployeeController.class).allEmployees()).withSelfRel());
     }
 
     @PostMapping(value = "/employees")
-    Employee newEmployee(@RequestBody Employee newEmployee){
+    public Employee newEmployee(@RequestBody Employee newEmployee){
         return this.employeeRepository.save(newEmployee);
     }
 
     //Single item
     @GetMapping(value = "/employee/{id}")
-    Employee getEmployee(@PathVariable Long id){
-        return this.employeeRepository.findById(id)
+    public EntityModel<Employee> getEmployee(@PathVariable Long id){
+        Employee employee = this.employeeRepository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
+        return this.employeeModelAssembler.toModel(employee);
     }
 
     @PutMapping(value = "/employees/{id}")
